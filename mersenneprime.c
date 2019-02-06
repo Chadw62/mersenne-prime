c65fc2f5488ef2850a36eda2483eb0cd1d66f932
Mr. Chad Harrison Wise <wwgw@iupmsd.iup.edu>
2019-02-06 11:02:18 -0500

    Update

--- mersenneprime.c
+++ mersenneprime.c
@@ -1,20 +1,26 @@
 #include <stdio.h>
 #include <math.h>
+#include <time.h>
 
 int main()
 {
 	int count;
 	int primes [50];
+	clock_t t;
+	int temp;
 
-	for(int i = 0;i < 20 ;i++){
-
-		int temp = pow(2,count) - 1;
+	for(int i = 0; i < 20 ;i++){
+		printf("1st loop");
+		t = clock();
+		temp = pow(2,count) - 1;
 		for(int j =0; j < sizeof(primes); j++){
 			if(temp % primes[j] == 0){
                 break;
             }
+			t = clock() - t;
+			double time = ((double)t)/CLOCKS_PER_SEC;
             primes[count++] = temp;
-            printf(temp + " is a M prime ");
+            printf("%f is a mersenne prime! %f seconds to execute", temp, time);
 		}
 	}
 	return 0;
